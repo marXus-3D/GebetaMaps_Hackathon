82be2272b54fd8e5af01fafdaa906eb4b6ff1b5f
marXus-3D <98558807+marXus-3D@users.noreply.github.com>
2024-11-09 20:33:47 +0300

    tailwind init

--- local_lens_web/src/App.js
+++ local_lens_web/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function Map() {
@@ -50,21 +50,13 @@ function SidePanel() {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex h-screen">
+      <div className="w-3/4 relative">
+        <Map />
+      </div>
+      <div className="w-1/4 bg-white shadow-lg">
+        <SidePanel />
+      </div>
     </div>
   );
 }
